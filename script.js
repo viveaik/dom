7276437d83e8086a7c7b4ef6d37b8b9bce75d4f4
viveaik <viveaik@yahoo.com>
2021-08-21 08:29:58 +0200

    getElementByTag

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+  var x = document.getElementById("div1");
+  x.getElementsByTagName("P")[0].style.backgroundColor = "red";
+  x.getElementsByTagName("P")[1].style.backgroundColor = "blue";
+
+}
